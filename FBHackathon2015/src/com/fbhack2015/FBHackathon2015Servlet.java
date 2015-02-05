162bfe7ff92418ec9b39ee1530821e73adb784cd
shantanu95 <gupta.shantanu95@gmail.com>
2015-02-05 15:51:48 +0800

    Initial Commit

---
+++ FBHackathon2015/src/com/fbhack2015/FBHackathon2015Servlet.java
@@ -0,0 +1,13 @@
+package com.fbhack2015;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class FBHackathon2015Servlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
